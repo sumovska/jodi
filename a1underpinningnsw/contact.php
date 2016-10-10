784499408d469615273339d3a2be308cfabf241d
Sergey Miron <sergey.miron@icloud.com>
2016-10-10 20:45:43 +0700

    fixed

--- a1underpinningnsw/contact.php
+++ a1underpinningnsw/contact.php
@@ -2,21 +2,23 @@
 
 
 // get posted data into local variables
-$EmailFrom = Trim(stripslashes($_POST['EmailFrom'])); 
+$EmailFrom = Trim(stripslashes($_POST['EmailFrom']));
 $EmailTo = "a1underpinning@gmail.com";
 $Subject = "You have a new website enquiry!";
-$Name = Trim(stripslashes($_POST['Name'])); 
-$Phone = Trim(stripslashes($_POST['Phone'])); 
-$Email = Trim(stripslashes($_POST['EmailFrom'])); 
-$Message = Trim(stripslashes($_POST['Message'])); 
+$Name = Trim(stripslashes($_POST['Name']));
+$Email = Trim(stripslashes($_POST['EmailFrom']));
+$Phone = Trim(stripslashes($_POST['Phone']));
+$Suburb = Trim(stripslashes($_POST['Suburb']));
+$Service = Trim(stripslashes($_POST['Service']));
+$Message = Trim(stripslashes($_POST['Message']));
 
 // validation
-$validationOK=true;
-if (Trim($EmailFrom)=="") $validationOK=false;
-if (Trim($Name)=="") $validationOK=false;
+$validationOK = true;
+if (Trim($EmailFrom) == "") $validationOK = false;
+if (Trim($Name) == "") $validationOK = false;
 if (!$validationOK) {
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
-  exit;
+	print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+	exit;
 }
 
 // prepare email body text
@@ -24,11 +26,17 @@ $Body = "";
 $Body .= "Name: ";
 $Body .= $Name;
 $Body .= "\n";
+$Body .= "Email: ";
+$Body .= $EmailFrom;
+$Body .= "\n";
 $Body .= "Phone: ";
 $Body .= $Phone;
 $Body .= "\n";
-$Body .= "EmailFrom: ";
-$Body .= $EmailFrom;
+$Body .= "Suburb: ";
+$Body .= $Suburb;
+$Body .= "\n";
+$Body .= "Service: ";
+$Body .= $Service;
 $Body .= "\n";
 $Body .= "Message: ";
 $Body .= $Message;
@@ -38,15 +46,15 @@ $Body .= "\n";
 $success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
 
 // redirect to success page 
-if ($success){
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=ok.html\">";
-}
-else{
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+if ($success) {
+	print "<meta http-equiv=\"refresh\" content=\"0;URL=ok.html\">";
+} else {
+	print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
 }
 
-if($_POST["gotcha"]!=""){
-	header("Location: {$_SERVER[HTTP_REFERER]}");exit;
+if ($_POST["gotcha"] != "") {
+	header("Location: {$_SERVER[HTTP_REFERER]}");
+	exit;
 }
 
 
