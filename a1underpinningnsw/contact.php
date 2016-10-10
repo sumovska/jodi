0b0661269f2df2b249f93f6b0f1f54e0e33f56f6
Sergey Miron <sergey.miron@icloud.com>
2016-10-10 20:38:25 +0700

    default

---
+++ a1underpinningnsw/contact.php
@@ -0,0 +1,55 @@
+<?php
+
+
+// get posted data into local variables
+$EmailFrom = Trim(stripslashes($_POST['EmailFrom'])); 
+$EmailTo = "a1underpinning@gmail.com";
+$Subject = "You have a new website enquiry!";
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Phone = Trim(stripslashes($_POST['Phone'])); 
+$Email = Trim(stripslashes($_POST['EmailFrom'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (Trim($EmailFrom)=="") $validationOK=false;
+if (Trim($Name)=="") $validationOK=false;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Phone: ";
+$Body .= $Phone;
+$Body .= "\n";
+$Body .= "EmailFrom: ";
+$Body .= $EmailFrom;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=ok.html\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+}
+
+if($_POST["gotcha"]!=""){
+	header("Location: {$_SERVER[HTTP_REFERER]}");exit;
+}
+
+
+?>	
+
+
