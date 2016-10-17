b348830ed36d31013d849d810c124f4c61488451
Sergey Miron <sergey.miron@icloud.com>
2016-10-17 23:37:31 +0700

    fixes

--- thelandscapeco/js/custom.js
+++ thelandscapeco/js/custom.js
@@ -1,180 +1,237 @@
 /*-----------------------------------------------------------------------------------
-/*
-/* Custom JS
-/*
------------------------------------------------------------------------------------*/
+ /*
+ /* Custom JS
+ /*
+ -----------------------------------------------------------------------------------*/
 
 /* Start Document */
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
-/*----------------------------------------------------*/
-/*	Main Navigation
-/*----------------------------------------------------*/
+    /*----------------------------------------------------*/
+    /*	Main Navigation
+     /*----------------------------------------------------*/
 
-	/* Menu */
-	(function() {
+    /* Menu */
+    (function () {
 
-		var $mainNav    = $('#navigation').children('ul');
+        var $mainNav = $('#navigation').children('ul');
 
-		$mainNav.on('mouseenter', 'li', function() {
-			var $this    = $(this),
-				$subMenu = $this.children('ul');
-			if( $subMenu.length ) $this.addClass('hover');
-			$subMenu.hide().stop(true, true).fadeIn(250);
-		}).on('mouseleave', 'li', function() {
-			$(this).removeClass('hover').children('ul').stop(true, true).fadeOut(50);
-		});
+        $mainNav.on('mouseenter', 'li', function () {
+            var $this = $(this),
+                $subMenu = $this.children('ul');
+            if ($subMenu.length) $this.addClass('hover');
+            $subMenu.hide().stop(true, true).fadeIn(250);
+        }).on('mouseleave', 'li', function () {
+            $(this).removeClass('hover').children('ul').stop(true, true).fadeOut(50);
+        });
 
-	})();
+    })();
 
-	/* Responsive Menu */
-	domready(function(){
+    /* Responsive Menu */
+    domready(function () {
 
-		selectnav('nav', {
-			label: 'Menu',
-			nested: true,
-			indent: '-'
-		});
+        selectnav('nav', {
+            label: 'Menu',
+            nested: true,
+            indent: '-'
+        });
 
-	});
+    });
 
-/*----------------------------------------------------*/
-/*	Isotope Portfolio Filter
-/*----------------------------------------------------*/
-	$(window).load(function(){
-		$('#portfolio-wrapper').isotope({
-			  itemSelector : '.portfolio-item',
-				layoutMode : 'fitRows'
-		});
-		$('#filters a.selected').trigger("click");
-	});
-	$('#filters a').click(function(e){
-		e.preventDefault();
+    /*----------------------------------------------------*/
+    /*	Isotope Portfolio Filter
+     /*----------------------------------------------------*/
+    $(window).load(function () {
+        $('#portfolio-wrapper').isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
+        $('#filters a.selected').trigger("click");
+    });
+    $('#filters a').click(function (e) {
+        e.preventDefault();
 
-		var selector = $(this).attr('data-option-value');
-		$('#portfolio-wrapper').isotope({ filter: selector });
+        var selector = $(this).attr('data-option-value');
+        $('#portfolio-wrapper').isotope({filter: selector});
 
-		$(this).parents('ul').find('a').removeClass('selected');
-		$(this).addClass('selected');
-	});
+        $(this).parents('ul').find('a').removeClass('selected');
+        $(this).addClass('selected');
+    });
 
 
-/*----------------------------------------------------*/
-/*	Back To Top Button
-/*----------------------------------------------------*/
+    /*----------------------------------------------------*/
+    /*	Back To Top Button
+     /*----------------------------------------------------*/
 
-	(function() {
-		var pxShow = 300;//height on which the button will show
-		var fadeInTime = 400;//how slow/fast you want the button to show
-		var fadeOutTime = 400;//how slow/fast you want the button to hide
-		var scrollSpeed = 400;//how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+    (function () {
+        var pxShow = 300;//height on which the button will show
+        var fadeInTime = 400;//how slow/fast you want the button to show
+        var fadeOutTime = 400;//how slow/fast you want the button to hide
+        var scrollSpeed = 400;//how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
-		jQuery(window).scroll(function(){
-			if(jQuery(window).scrollTop() >= pxShow){
-				jQuery("#backtotop").fadeIn(fadeInTime);
-			}else{
-				jQuery("#backtotop").fadeOut(fadeOutTime);
-			}
-		});
+        jQuery(window).scroll(function () {
+            if (jQuery(window).scrollTop() >= pxShow) {
+                jQuery("#backtotop").fadeIn(fadeInTime);
+            } else {
+                jQuery("#backtotop").fadeOut(fadeOutTime);
+            }
+        });
 
-		jQuery('#backtotop a').click(function(){
-			jQuery('html, body').animate({scrollTop:0}, scrollSpeed);
-			return false;
-		});
-	})();
+        jQuery('#backtotop a').click(function () {
+            jQuery('html, body').animate({scrollTop: 0}, scrollSpeed);
+            return false;
+        });
+    })();
 
-/*----------------------------------------------------*/
-/*	Contact Form
-/*----------------------------------------------------*/
+    /*----------------------------------------------------*/
+    /*	Contact Form
+     /*----------------------------------------------------*/
 
-(function() {
-var animateSpeed=300;
-var emailReg = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z0-9.-]{2,4}$/;
+    (function () {
+        var animateSpeed = 300;
+        var emailReg = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z0-9.-]{2,4}$/;
 
-	// Validating
+        // Validating
+        function validateName(name) {
+            if (name.val() == '') {
+                name.addClass('validation-error', animateSpeed);
+                if (result) {
+                    $('.error-message').remove();
+                    $('<div/>').addClass('error-message').html('<div class="alert pink">Please enter your name</div>').insertAfter($('.success-message'));
+                }
+                return false;
+            }
+            else {
+                name.removeClass('validation-error', animateSpeed);
+                $('.error-message').remove();
+                result = true;
+                return true;
+            }
+        }
 
-	function validateName(name) {
-		if (name.val()=='') {name.addClass('validation-error',animateSpeed); return false;}
-		else {name.removeClass('validation-error',animateSpeed); return true;}
-	}
+        function validateEmail(email, regex) {
+            if (!regex.test(email.val())) {
+                email.addClass('validation-error', animateSpeed);
+                if (result) {
+                    $('.error-message').remove();
+                    $('<div/>').addClass('error-message').html('<div class="alert pink">Please enter your email</div>').insertAfter($('.success-message'));
+                }
+                return false;
+            }
+            else {
+                email.removeClass('validation-error', animateSpeed);
+                $('.error-message').remove();
+                result = true;
+                return true;
+            }
+        }
 
-	function validateEmail(email,regex) {
-		if (!regex.test(email.val())) {email.addClass('validation-error',animateSpeed); return false;}
-		else {email.removeClass('validation-error',animateSpeed); return true;}
-	}
+        function validateMessage(message) {
+            if (message.val() == '') {
+                message.addClass('validation-error', animateSpeed);
+                if (result) {
+                    $('.error-message').remove();
+                    $('<div/>').addClass('error-message').html('<div class="alert pink">Please enter your message</div>').insertAfter($('.success-message'));
+                }
+                return false;
+            }
+            else {
+                message.removeClass('validation-error', animateSpeed);
+                $('.error-message').remove();
+                result = true;
+                return true;
+            }
+        }
 
-	function validateMessage(message) {
-		if (message.val()=='') {message.addClass('validation-error',animateSpeed); return false;}
-		else {message.removeClass('validation-error',animateSpeed); return true;}
-	}
+        var result = true;
 
-	$('#send').click(function() {
+        $('#send').click(function () {
 
-		var result=true;
+            result = true;
 
-		var name = $('input[name=name]');
-		var email = $('input[name=email]');
-		var message = $('textarea[name=message]');
+            var name = $('input[name=name]');
+            var email = $('input[name=email]');
+            var message = $('textarea[name=message]');
 
-		// Validate
-		if(!validateName(name)) result=false;
-		if(!validateEmail(email,emailReg)) result=false;
-		if(!validateMessage(message)) result=false;
+            // Validate
+            if (!validateName(name)) result = false;
+            if (!validateEmail(email, emailReg)) result = false;
+            if (!validateMessage(message)) result = false;
 
-		if(result==false) return false;
+            if (result == false) return false;
 
-		// Data
-		var data = 'name=' + name.val() + '&email=' + email.val() + '&message='  + encodeURIComponent(message.val());
+            // Data
+            var data = 'name=' + name.val() + '&email=' + email.val() + '&message=' + encodeURIComponent(message.val());
 
-		// Disable fields
-		$('.text').attr('disabled','true');
+            // Disable fields
+            $('.text').attr('disabled', 'true');
 
-		// Loading icon
-		$('.loading').show();
+            // Loading icon
+            $('.loading').show();
 
-		// Start jQuery
-		$.ajax({
+            // Start jQuery
+            $.ajax({
 
-			// PHP file that processes the data and send mail
-			url: "contact.php",
+                // PHP file that processes the data and send mail
+                url: "contact.php",
 
-			// GET method is used
-			type: "GET",
+                // GET method is used
+                type: "GET",
 
-			// Pass the data
-			data: data,
+                // Pass the data
+                data: data,
 
-			//Do not cache the page
-			cache: false,
+                //Do not cache the page
+                cache: false,
 
-			// Success
-			success: function (html) {
-				if (html==1) {
-					// Loading icon
-					$('.loading').fadeOut('slow');
+                // Success
+                success: function (html) {
+                    if (html == 1) {
+                        // Loading icon
+                        $('.loading').fadeOut('slow');
 
-					//show the success message
-					$('.success-message').slideDown('slow');
+                        //show the success message
+                        $('.success-message').slideDown('slow');
 
-					// Disable send button
-					$('#send').attr('disabled',true);
-				}
-				else {
-					$('.loading').fadeOut('slow');
-					alert('Sorry, unexpected error. Please try again later.');
-				}
-			}
-		});
+                        // Disable send button
+                        $('#send').attr('disabled', true);
+                    }
+                    else {
+                        $('.loading').fadeOut('slow');
+                        alert('Sorry, unexpected error. Please try again later.');
+                    }
+                }
+            });
 
-		return false;
+            return false;
 
-	});
+        });
 
-	$('input[name=name]').blur(function(){validateName($(this));});
-	$('input[name=email]').blur(function(){validateEmail($(this),emailReg); });
-	$('textarea[name=message]').blur(function(){validateMessage($(this)); });
+        $('input[name=name]').blur(function () {
+            validateName($(this));
+        }).focus(function () {
+            if ($(this).hasClass('validation-error')) {
+                validateName($(this));
+            }
+        });
+        $('input[name=email]').blur(function () {
+            validateEmail($(this), emailReg);
+        }).focus(function () {
+            console.log('focus');
+            if ($(this).hasClass('validation-error')) {
+                console.log('a');
+                validateEmail($(this), emailReg);
+            }
+        });
+        $('textarea[name=message]').blur(function () {
+            validateMessage($(this));
+        }).focus(function () {
+            if ($(this).hasClass('validation-error')) {
+                validateMessage($(this));
+            }
+        });
 
-})();
+    })();
 
-/* End Document */
+    /* End Document */
 });
